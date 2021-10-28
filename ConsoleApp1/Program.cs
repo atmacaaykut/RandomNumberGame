f6c46e9d41fd8094482b287b1bf899fab342098f
aykut <73406165+atmacaaykut@users.noreply.github.com>
2021-10-28 20:46:11 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            int cpuNumber = 0;
+            int userNumber = 1;
+            int counter = 0;
+            while (cpuNumber != userNumber)
+            {
+                Console.Write("Please enter a number between 1-20: ");
+                cpuNumber = r.Next(1, 21);
+                userNumber = Convert.ToInt16(Console.ReadLine());
+                counter++;
+
+            }
+            Console.WriteLine("You found " + counter + ". try");
+           
+        
+        }
+    }
+}
